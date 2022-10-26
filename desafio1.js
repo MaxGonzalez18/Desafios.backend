b9b9f38b57bc0c80299f01054946823803263c86
MaxGonzalez18 <116604533+MaxGonzalez18@users.noreply.github.com>
2022-10-26 19:17:55 -0300

    Create desafio1.js

---
+++ desafio1.js
@@ -0,0 +1,36 @@
+class usuario{
+constructor (nombre, apellido, libros, mascotas){
+this.nombre=nombre
+this.apellido=apellido;
+this.libros= [libros] 
+this.mascotas=[mascotas];
+}
+getFullName(){
+return console.log (`NOMBRE COMPLETO: ${this.nombre} ${this.apellido}`) 
+}
+addMascotas (nombreMascotas){
+this.mascotas.push(nombreMascotas);
+}
+countMascotas (){
+return console.log(this.mascotas.length);
+}
+addBook (nombreDelLibro,autorDelLibro){
+this.libros.push({
+Titulo: nombreDelLibro, 
+Autor:autorDelLibro}); 
+} 
+getNameBook(){
+let nameBook = this.libros.map(function(titulos){
+return titulos.Titulo
+})
+console.log (nameBook) 
+}
+}
+const usuario1 = new usuario("Maximiliano", "Gonzalez", {Titulo:"El resplandor" , Autor:"Stephen King"}, "Spike")
+usuario1.addBook ("Cien años de soledad" , "Gabriel Garcia Marquez")
+usuario1.addBook("el Hobbit", "JRR Tolkien")
+usuario1.addMascotas("blanquito")
+usuario1.getFullName()
+usuario1.countMascotas()
+usuario1.getNameBook()
+console.log (usuario1)
